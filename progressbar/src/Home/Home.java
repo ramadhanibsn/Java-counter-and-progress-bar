481ac3b16b1fc3f90e6aae3a2a445f5a2dbba7a6
ramadhanibsn <38847619+ramadhanibsn@users.noreply.github.com>
2020-05-16 00:15:13 +0700

    Add files via upload

---
+++ progressbar/src/Home/Home.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Home;
+
+/**
+ *
+ * @author User
+ */
+public class Home {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Progress prog = new Progress();
+        prog.setVisible(true);
+    }
+    
+}
